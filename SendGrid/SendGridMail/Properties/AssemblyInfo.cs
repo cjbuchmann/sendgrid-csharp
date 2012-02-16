c2aee456c6c720f0010144460c3ce56e999ebf7b
CJ Buchmann <cj.buchmann@sendgrid.com>
2012-02-15 16:09:15 -0800

    Added Signed Assembly info to the mail and test libraries

--- SendGrid/SendGridMail/Properties/AssemblyInfo.cs
+++ SendGrid/SendGridMail/Properties/AssemblyInfo.cs
@@ -19,17 +19,21 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
+[assembly: InternalsVisibleTo("Tests, PublicKey=" +
+"002400000480000094000000060200000024000052534131000400000100010007601d59f4e21d" +
+"2e6d7790212a9c94b9514fe23a6313b652c8cc4216bef90eed375d4a5820d0c80206534498d6cf" +
+"ee33b377b8d12b6d46c563367641b10294009ec0ebdca993ab80aced936c3f089727b429565f64" +
+"4ffffc42396ff183902ace3ddf060bf947d33cac98f924bcc919f17d7bd300cf5be3c8dac18870" +
+"302e4fab")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2, PublicKey=" +
+"0024000004800000940000000602000000240000525341310004000001000100c547cac37abd99" + 
+"c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d9266654" +
+"753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb8e2121380fd1d9d46" +
+"ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a7d3113e92484c" + 
+"f7045cc7")]
+
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("193fa200-8430-4206-aacd-2d2bb2dfa6cf")]
-
-[assembly: InternalsVisibleTo("Tests")]
-[assembly:InternalsVisibleTo("DynamicProxyGenAssembly2,"  +
-"1310004000001000100c547cac37abd99c8db225ef2f6c8a3602f3b" +
-"PublicKey=002400000480000094000000060200000024000052534" +
-"3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d926665" +
-"4753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb" +
-"4f64bcb8e2121380fd1d9d46ad2d92d2d15605093924cceaf74c486" +
-"1eff62abf69b9291ed0a340e113be11e6a7d3113e92484cf7045cc7")]
 
 // Version information for an assembly consists of the following four values:
 //
